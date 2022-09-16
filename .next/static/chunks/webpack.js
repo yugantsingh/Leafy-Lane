f10575e77350d4cf7769ec23b9b204c5a689af94
yugantx@gmail.com <yugantx@gmail.com>
2022-09-16 12:22:53 +0530

    Mainting the state of the fetched data using Context and staticprops and static paths

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -150,7 +150,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "14d32e8f117cf948"; }
+/******/ 		__webpack_require__.h = function() { return "950b867d5a19cb7d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
